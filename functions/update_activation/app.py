7c3169570f804d0dd9562dd49359985ded021d9f
中村昌克 <nakamura.masakatsu@gmail.com>
2020-12-07 12:24:09 +0900

    利用されているアクティベーションは削除しない

--- functions/update_activation/app.py
+++ functions/update_activation/app.py
@@ -5,24 +5,52 @@ import re
 ssm = boto3.client('ssm')
 ecs = boto3.client('ecs', 'ap-northeast-1')
 
+def instance_avairable(activation_id):
+  instances = []
+  params = {
+    'Filters': [
+      {
+        'Key': 'PingStatus',
+        'Values': [ 'Online' ]
+      },
+      {
+        'Key': 'ActivationIds',
+        'Values': [ activation_id ]
+      }
+    ]
+  }
+  while True:
+    res = ssm.describe_instance_information(**params)
+    instances += res['InstanceInformationList']
+    if 'NextToken' in res:
+      params['NextToken'] = res['NextToken']
+    else:
+      break
+  return(len(instances) > 0)
+
+def activations():
+  acts = []
+  params = {}
+  while True:
+    res = ssm.describe_activations(**params)
+    acts += res['ActivationList']
+    if 'NextToken' in res:
+      params['NextToken'] = res['NextToken']
+    else:
+      break
+  return(acts)
+
 def lambda_handler(event, context):
   try:
-    acts = []
-    params = {}
-    while True:
-      res = ssm.describe_activations(**params)
-      acts += res['ActivationList']
-      if 'NextToken' in res:
-        params['NextToken'] = res['NextToken']
-      else:
-        break
-
-    for act in acts:
+    for act in activations():
       expire_date = datetime.datetime.fromisoformat(act['ExpirationDate'])
       match = re.search(r'^(prod|stg|stg\d+)-knockme-activation-.*$', act['Description'])
       # 対象のアクティベーションが24時間以内に失効する場合、更新する。
       if match and expire_date < datetime.datetime.now() + datetime.timedelta(hours = 24):
-        ssm.delete_activation(ActivationId=act['ActivationId'])
+        # 当該アクティベーションに紐づくインスタンスがない場合、アクティベーションを削除する
+        if not instance_avairable(act['ActivationId']):
+          ssm.delete_activation(ActivationId=act['ActivationId'])
+
         activation_name = match[0]
         stage = match[1]
 
